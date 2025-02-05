00bde676d3460425da8692176cfadc5aecc514b0
kbellouard <killian.bellouard@getnobullshit.com>
2025-02-05 14:21:24 +0100

    fix prev

--- main.go
+++ main.go
@@ -102,7 +102,7 @@ func corsMiddleware(next http.Handler) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
 		next.ServeHTTP(w, r)
 	})
 }
